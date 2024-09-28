0f3dd408268a10f7c23bc5c65ef7a16847f3cd98
Maksim <maxmag2007@yandex.ru>
2024-09-29 02:05:43 +0300

    таблица

--- TGBot/telegram_bot.py
+++ TGBot/telegram_bot.py
@@ -1,4 +1,8 @@
-from ProjectClass import bot, ProjectReplyKeyboard
+from ProjectClass import bot, ProjectReplyKeyboard, ProjectInlineKeyboard, MenuPageListQuestion, QuestionInlineKeyboard
+import math
+
+
+AMMOUNT_QUESTION_IN_ONE_PAGE = 20
 
 
 # python TGBot\telegram_bot.py
@@ -15,7 +19,7 @@ def welcome_func_bot(message):
         bot.register_next_step_handler(message, set_login_func_bot)
     else:
         # если существует, то присылаем ему меню кнопок (каждый список обозначает одну строку)
-        keyboard = ProjectReplyKeyboard(True, ['Задать вопрос', 'Список вопросов', '/my_login', '/my_password', '/start'], row_width=2)
+        keyboard = ProjectReplyKeyboard(True, ['Задать вопрос', 'Список вопросов', 'Список моих вопросов', '/start'], row_width=2)
         bot.send_message(message.chat.id, 'Здравствуйте!', reply_markup=keyboard)
 
 
@@ -31,7 +35,7 @@ def set_login_func_bot(message):
 def set_password_func_bot(message):
     user_password = message.text
     print(user_password)
-    keyboard = ProjectReplyKeyboard(True, ['Задать вопрос', 'Список вопросов', '/my_login', '/my_password', '/start'], row_width=2)
+    keyboard = ProjectReplyKeyboard(True, ['Задать вопрос', 'Список вопросов', 'Список моих вопросов', '/start'], row_width=2)
     bot.send_message(message.chat.id, 'Вы зарегестрированы!', reply_markup=keyboard)
 
 
@@ -45,8 +49,61 @@ def ask_question_func_bot(message):
 @bot.message_handler(func=lambda message: message.text == 'Список вопросов')
 def list_question_func_bot(message):
     list_question = [f'{i} - question in  bot lalalalal' for i in range(57)]
-    bot.send_message(message.chat.id, 'Список текущих вопросов:')
     
+    keyboard = MenuPageListQuestion(list_question=list_question, row_width=2,
+                                    current_page=0, ammount_question_in_one_page=AMMOUNT_QUESTION_IN_ONE_PAGE)
+    bot.send_message(chat_id=message.chat.id, text=f'Список текущих вопросов. Страница 1/{math.ceil(len(list_question) / AMMOUNT_QUESTION_IN_ONE_PAGE)}', reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'back_page_list_question')
+def list_question_back_page_func_bot(call):
+    list_question = [f'{i} - question in  bot lalalalal' for i in range(57)]
+    current_page = MenuPageListQuestion.give_current_page_by_message_text(message_text=call.message.text)
+
+    if current_page > 0:
+        current_page -= 1
+
+        keyboard = MenuPageListQuestion(list_question=list_question, row_width=2,
+                                        current_page=current_page, ammount_question_in_one_page=AMMOUNT_QUESTION_IN_ONE_PAGE)
+
+        bot.edit_message_text(text=f'Список текущих вопросов. Страница {current_page + 1}/{math.ceil(len(list_question) / AMMOUNT_QUESTION_IN_ONE_PAGE)}',
+                              chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'next_page_list_question')
+def list_question_next_page_func_bot(call):
+    list_question = [f'{i} - question in  bot lalalalal' for i in range(57)]
+    current_page = MenuPageListQuestion.give_current_page_by_message_text(message_text=call.message.text)
+
+    if current_page < math.ceil(len(list_question) / AMMOUNT_QUESTION_IN_ONE_PAGE) - 1:
+        current_page += 1
+
+        keyboard = MenuPageListQuestion(list_question=list_question, row_width=2,
+                                        current_page=current_page, ammount_question_in_one_page=AMMOUNT_QUESTION_IN_ONE_PAGE)
+
+        bot.edit_message_text(text=f'Список текущих вопросов. Страница {current_page + 1}/{math.ceil(len(list_question) / AMMOUNT_QUESTION_IN_ONE_PAGE)}',
+                              chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.endswith('_clicked_element_list_question'))
+def show_question_in_list_question_func_bot(call):
+    list_question = [f'{i} - question in  bot lalalalal' for i in range(57)]
+    index_question = MenuPageListQuestion.give_index_question(call.data)
+
+    keyboard = QuestionInlineKeyboard(row_width=2)
+    bot.edit_message_text(text=f'Вопрос №{index_question + 1}:\n{list_question[index_question]}', chat_id=call.message.chat.id, message_id=call.message.message_id,
+                          reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'back_to_list_question')
+def list_question_func_bot(call):
+    list_question = [f'{i} - question in bot lalalalal' for i in range(57)]
+    index_question = QuestionInlineKeyboard.give_index_question_by_message_text(message_text=call.message.text)
+    current_page = MenuPageListQuestion.give_current_page_by_index_question(index_question=index_question, ammount_question_in_one_page=AMMOUNT_QUESTION_IN_ONE_PAGE)
+
+    keyboard = MenuPageListQuestion(list_question=list_question, row_width=2, current_page=current_page, ammount_question_in_one_page=AMMOUNT_QUESTION_IN_ONE_PAGE)
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                          text=f'Список текущих вопросов. Страница {current_page + 1}/{math.ceil(len(list_question) / AMMOUNT_QUESTION_IN_ONE_PAGE)}', reply_markup=keyboard)
 
 
 if __name__ == '__main__':
