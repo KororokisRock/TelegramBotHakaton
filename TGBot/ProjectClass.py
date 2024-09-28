daa010149ea092197e7188debe01be304411980f
glinochka <118928386+glinochka@users.noreply.github.com>
2024-09-28 16:23:45 +0500

    'env2'

--- TGBot/ProjectClass.py
+++ TGBot/ProjectClass.py
@@ -1,6 +1,6 @@
 import telebot
-from config import TOKEN
-
+from decouple import config
+TOKEN = config('TOKEN',default='')
 class ProjectBot(telebot.TeleBot):
     def __init__(self, token):
         super().__init__(token)
