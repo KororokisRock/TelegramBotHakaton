b9ba181f3e7c3467d4bd5035ac7aba20575f32ef
Maksim <maxmag2007@yandex.ru>
2024-09-28 16:53:40 +0300

    tg_bot func register

--- TGBot/telegram_bot.py
+++ TGBot/telegram_bot.py
@@ -3,7 +3,19 @@ from ProjectClass import bot
 
 @bot.message_handler(commands=['start'])
 def welcome_func_bot(message):
-    bot.send_message(message.chat.id, 'Здравствуйте! Этот бот сделан для хакатона.')
+    bot.send_message(message.chat.id, 'Здравствуйте! Вы не зарегестрированы. Введите логин: ')
+    bot.register_next_step_handler(message, set_login_func_bot)
+
+
+def set_login_func_bot(message):
+    user_login = message.text
+    bot.send_message(message.chat.id, 'А теперь введите пароль:')
+    bot.register_next_step_handler(message, set_password_func_bot)
+
+
+def set_password_func_bot(message):
+    user_password = message.text
+    bot.send_message(message.chat.id, 'Вы зарешестрированы!')
 
 
 if __name__ == '__main__':
